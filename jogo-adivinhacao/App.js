d5e23188bd1eb2ed9754ae50f7276065c956e642
Thallys <randytar5@gmail.com>
2021-11-09 11:49:15 -0300

    mudanças

--- jogo-adivinhacao/App.js
+++ jogo-adivinhacao/App.js
@@ -1,28 +1,21 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { View, Text } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import Initial from './src/pages/InitialPage';
+import { StyleSheet, Text, View } from 'react-native';
 
-function DetailsScreen() {
+export default function App() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
 
-const Stack = createNativeStackNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Details">
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-
-  );
-};
-
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
