cf5f4cd1a873a7952c2bbf485825c22ad8538c58
Thallys <randytar5@gmail.com>
2021-11-09 14:31:15 -0300

    iniciando projeto denovo

---
+++ jogo-adivinhacao/src/pages/GamePage/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+import Estilos from "./styles";
+
+export default function Game(){
+    return(
+        <View>
+            <Text>Game Page</Text>
+        </View>
+    )
+}
